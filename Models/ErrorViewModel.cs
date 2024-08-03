f743f890e2cef78466e0652c4e678ebbc8af12bf
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-08-03 12:34:20 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiddleWareInMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
